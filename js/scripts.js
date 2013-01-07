6574bb5d39e6fa4895b9542dd2edfe0d3c366948
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2013-01-07 22:52:34 +0530

    GA in globa js file. Refactored buses object.

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+var buses = {"buses": [{
+   "id" : "123A",
+   "orig": "Pune Railway Station",
+   "short_orig" : "Pune Stn",
+   "dest": "Vagholi Gaon",
+   "short_dest" : "Wagholi",
+   "waypoints": [{"n": "Wadia College", "t": "10min"},
+      {"n": "Bund Garden", "t": "10min"},
+      {"n": "Yerawada", "t": "10min"},
+      {"n": "Inorbit Mall", "t": "10min"},
+      {"n": "Wagholi", "t": "10min"}],
+   "timings": ['6 AM', '12 PM', '5PM'],
+   "durn" : '60 min'
+},
+{
+   "id" : "56B",
+   "orig": "Pune Municipal Corporation",
+   "short_orig" : "PMC",
+   "dest": "Balewadi",
+   "short_dest" : "Balewadi",
+   "waypoints": [{"n": "E-Square", "t": "10min"},
+      {"n": "Symantec", "t": "10min"},
+      {"n": "Balewadi", "t": "10min"}],
+   "timings": ['6 AM', '12 PM', '5 PM'],
+   "durn" : '50 min'
+}]
+};
+
+
+// Google Analytics code begin ======================================================
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-37444267-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+// Google Analytics code end ========================================================
